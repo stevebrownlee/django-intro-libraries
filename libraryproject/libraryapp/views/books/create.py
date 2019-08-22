e4ead7123898d6d53e09a15e105dfd3241fee4eb
Steve Brownlee <chortlehoort@gmail.com>
2019-08-21 21:09:35 -0500

    Using insert statement instead of save()

--- libraryproject/libraryapp/views/books/create.py
+++ libraryproject/libraryapp/views/books/create.py
@@ -10,7 +10,14 @@ from ..connection import Connection
 
 @login_required
 def create_book(request):
+
     if request.method == 'GET':
+        '''
+        Check if the request method is a GET, and if so, get all the
+        libraries in the system so that the book form has a <select>
+        element with all the libraries as <option> elements
+        '''
+
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = model_factory(Library)
             db_cursor = conn.cursor()
@@ -29,31 +36,26 @@ def create_book(request):
         return render(request, template_name, {'all_libraries': all_libraries})
 
     elif request.method == 'POST':
+        '''
+        Check if the request method is a POST, which means the user
+        filled out the form and clicked the submit button. Then extract
+        all the form fields from the request, and assign those values
+        to a new Book instance. Then invoke the `.save()` method to
+        insert the data into the database.
+        '''
+
         form_data = request.POST
 
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = model_factory(Library)
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
-            select
-                l.id,
-                l.title,
-                l.address
-            from libraryapp_library l
-            where l.id = ?
-            """, form_data["location"])
-
-            library = db_cursor.fetchone()
-
-        new_book = Book(
-            librarian=request.user.librarian,
-            title=form_data['title'],
-            author=form_data['author'],
-            isbn=form_data['isbn'],
-            year_published=form_data['year_published'],
-            location=library,
-        )
-        new_book.save()
+             INSERT INTO libraryapp_book
+             (title, author, isbn, year_published, location_id, librarian_id)
+             values (?, ?, ?, ?, ?, ?)
+             """,
+            (form_data['title'], form_data['author'],
+                form_data['isbn'], form_data['year_published'],
+                request.user.librarian.id, form_data["location"]))
 
         return redirect(reverse('libraryapp:list_books'))
